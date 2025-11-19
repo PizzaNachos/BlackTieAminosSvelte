a64e3418543c742969389e15a3ece591e82d427b
Michael <michael@colorado4x4.net>
2025-11-19 15:07:50 -0700

    fixed logo

--- src/lib/icons/Logo.svelte
+++ src/lib/icons/Logo.svelte
@@ -1,53 +1,53 @@
 <script>
-	export let width = 'auto';
-	export let height = 'auto';
+  export let width = "auto";
+  export let height = "auto";
 </script>
 
-<img src="/logo.png" alt="Logo" style="height: 100%; width: auto" />
+<img src="/logo.png" alt="Logo" style="height:3em;" />
 
 <style>
-	.cls-1,
-	.cls-2,
-	.cls-3 {
-		fill: inherit;
-	}
+  .cls-1,
+  .cls-2,
+  .cls-3 {
+    fill: inherit;
+  }
 
-	.cls-2 {
-		font-size: 89.13px;
-		font-family:
-			FranklinGothic-Medium,
-			Franklin Gothic Medium;
-		font-weight: 500;
-	}
+  .cls-2 {
+    font-size: 89.13px;
+    font-family:
+      FranklinGothic-Medium,
+      Franklin Gothic Medium;
+    font-weight: 500;
+  }
 
-	.cls-3 {
-		font-size: 236.52px;
-		stroke-width: 3px;
-		font-family:
-			TrebuchetMS,
-			Trebuchet MS;
-	}
+  .cls-3 {
+    font-size: 236.52px;
+    stroke-width: 3px;
+    font-family:
+      TrebuchetMS,
+      Trebuchet MS;
+  }
 
-	.cls-3,
-	.cls-5 {
-		fill: inherit;
-		stroke-miterlimit: 10;
-	}
+  .cls-3,
+  .cls-5 {
+    fill: inherit;
+    stroke-miterlimit: 10;
+  }
 
-	.cls-4 {
-		letter-spacing: -0.06em;
-	}
+  .cls-4 {
+    letter-spacing: -0.06em;
+  }
 
-	.cls-5 {
-		fill: inherit;
-		stroke-width: 4px;
-	}
+  .cls-5 {
+    fill: inherit;
+    stroke-width: 4px;
+  }
 
-	.cls-6 {
-		fill: #fff;
-	}
+  .cls-6 {
+    fill: #fff;
+  }
 
-	.cls-7 {
-		fill: inherit;
-	}
+  .cls-7 {
+    fill: inherit;
+  }
 </style>
